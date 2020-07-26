0fc72e5e1cb2db4a3646468cfb19f6b3e307c15c
Blake Gearin <blakegearin@gmail.com>
2020-07-26 00:17:47 -0500

    Base tests for popup

--- test/popup.test.js
+++ test/popup.test.js
@@ -1,7 +1,16 @@
-const Person = require('../js/popup');
+const { chrome } = require('jest-chrome');
+require('../js/popup');
 
 describe('on startup', () => {
   it('should call chrome.storage.local.get once', () => {
     expect(chrome.storage.local.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call chrome.i18n.getMessage 19 times', () => {
+    expect(chrome.i18n.getMessage).toHaveBeenCalledTimes(19);
+  });
+
+  it('should have listener on chrome.storage.onChanged', () => {
+    expect(chrome.storage.onChanged.hasListeners()).toBe(true)
   });
 });
